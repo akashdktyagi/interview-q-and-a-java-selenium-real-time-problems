ffacf72ecb49803621bcc7ea4d8b3334375cb64c
akashtyagi <akashdktyagi@gmail.com>
2021-03-29 18:07:33 +0800

    jackson mapper dummy file added

---
+++ src/main/java/com/automationfraternity/_27_JacksonObjectMapper.java
@@ -0,0 +1,8 @@
+package com.automationfraternity;
+
+public class _27_JacksonObjectMapper {
+
+    //https://www.baeldung.com/jackson-object-mapper-tutorial
+    //https://dzone.com/articles/converting-json-to-pojos-using-java
+    //https://www.vogella.com/tutorials/DesignPatternBuilder/article.html
+}
